7e0c1984a1c6cbcb681b019a1e8364506c5c16a2
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-07-04 19:36:05 -0300

    ,,

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const perguntas =[
         alternativas:[
             " Evito ao máximo contato com pessoas, me restringindo ao contato somente com os familiares que moram em minha casa.",
             " Acredito que isso não é nada sério afinal não tem nada comprovado e me reuno com diversas pessoas em local público"
-        ], 
-    },
+        ]
+    }
     {
         enunciado: "Assim que o primeiro caso desse vírus é detectado em seu estado, a organização de saúde recomenda o uso de máscara para minimizar o contágio. O que você faria neste caso?",
         alternativas: [
             "Uso máscara sempre que tenha mais alguem comigo no mesmo ambiente para evitar contágio.", 
             "Continuaria me reunindo sem preocupações pois só estou com pessoas que confio e conheço."
-        ],
-    },
+        ]
+    }
 ];
 caixaPerguntas.textContent = "Eu consigo voarrrrr"
