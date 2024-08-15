c7506f23d872c1e39539bf23dc0bc616906b7521
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-08-15 19:34:23 -0300

    ok

--- script.js
+++ script.js
@@ -4,45 +4,76 @@ const caixaAlternativas= document.querySelector(".caixa-alternativas");
 const caixaResultado= document.querySelector(".caixa-resultado");
 const textoResultado= document.querySelector(".texto-resultado");
 
-
-const perguntas =[
+const perguntas = [
     {
-        enunciado:"Um vírus está deixando muitos doentes e a organização de saude indica que para minimizar o contágio seja necessário diminuir o contato entre as pessoas. O que você faz ao ouvir estas recomendaçãoes?",
-        alternativas:[
-            " Evito ao máximo contato com pessoas, me restringindo ao contato somente com os familiares que moram em minha casa.",
-            " Acredito que isso não é nada sério afinal não tem nada comprovado e me reuno com diversas pessoas em local público"
+        enunciado: "Um vírus está deixando muitos doentes e a organização de saúde indica que para minimizar o contágio seja necessário diminuir o contato entre as pessoas. O que você faz ao ouvir estas recomendaçãoes?",
+        alternativas: [
+            {
+                texto: "Evito ao máximo contato com pessoas, me restringindo ao contato somente com os familiares que moram em minha casa.",
+                afirmacao: " O isolamento social logo no surgimento do vírus pode diminuir ou atrasar o contágio entre a população."
+            },
+
+            {
+                texto: "Acredito que isso não é nada sério afinal não tem nada comprovado e me reuno com diversas pessoas em local público",
+                afirmacao : " O não isolamento social logo no surgimento do vírus pode ter agilizado o contágio entre a população"
+            }   
+            
         ]
     },
     {
         enunciado: "Assim que o primeiro caso desse vírus é detectado em seu estado, a organização de saúde recomenda o uso de máscara para minimizar o contágio. O que você faria neste caso?",
         alternativas: [
-            "Uso máscara sempre que tenha mais alguem comigo no mesmo ambiente para evitar contágio.", 
-            "Continuaria me reunindo sem preocupações pois só estou com pessoas que confio e conheço."
+            {
+                texto: "Uso máscara sempre que tenha mais alguem comigo no mesmo ambiente para evitar contágio.",
+                afirmacao: "Afirmação da alternativa 1"
+            },
+            {
+                texto: "Continuaria me reunindo sem preocupações pois só estou com pessoas que confio e conheço.",
+                afirmacao: "Texto da afirmação 2"
+            }
+            
         ]
-    },
+    }, 
     {
         enunciado: "O vírus já matou muitas pessoas e os cientistas após pesquisas desenvolveram uma vacina para protejer as pessoas do vírus e assim diminuir o numero de mortes causadas por eles. Neste caso o que você faria?",
         alternativas: [
-            "Iria me vacinar e divulgaria para que todos meus conhecidos também buscassem a vacina.", 
-            "Teria medo e não me vacinaria, tambem iria aconcelhar meus conhecidos a não vacinar pois acredito que a vacina é mais perigosa do que o vírus."
+            {
+                texto: "Iria me vacinar e divulgaria para que todos meus conhecidos também buscassem a vacina.",
+                afirmacao: "AFIRMATIVA RESPOSTA 1"
+            },
+            {
+                texto: "Teria medo e não me vacinaria, tambem iria aconcelhar meus conhecidos a não vacinar pois acredito que a vacina é mais perigosa do que o vírus.",
+                afirmacao: "AFIRMATIVA RESPOSTA 2."
+            }
         ]
-    },
+    }
 ];
-let atual=0;
+
+
+let atual = 0;
 let perguntaAtual;
+let historiaFinal="";
 
 function mostraPergunta(){
-    perguntaAtual=perguntas[atual];
+    perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntas[atual].enunciado;
-    caixaAlternativas.textContent="";
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas=document.createElement ("button");
-        botaoAlternativas.textContent=alternativa;
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada (opcaoSelecionada){
+    const afirmacao=opcaoSelecionada.afirmacao;
+    historiaFinal=afirmacao;
+    atual++;
+    mostraPergunta();
 }
 mostraPergunta();
